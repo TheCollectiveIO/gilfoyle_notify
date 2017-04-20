462531828d8c1cf08eb5a4f2d365b3580212a48d
concordiasummit <rdunn@concordia.net>
2017-04-20 13:41:19 -0400

    install tweak

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,5 +3,5 @@ sudo apt-get install inotify-tools
 sudo mv gilfoyle.sh /usr/bin
 sudo chown root:root /usr/bin/gilfoyle.sh
 sudo chmod +x /usr/bin/gilfoyle.sh
-sudo sed -i "\$i sh /usr/bin/gilfoyle_notify/gilfoyle.sh" /etc/rc.local
+sudo sed -i "\$i sh /usr/bin/gilfoyle.sh" /etc/rc.local
 
